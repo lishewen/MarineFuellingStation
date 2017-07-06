e456734c95fbde14fa70171ea5698816c42082f9
黎摄文 <lishewen@gmail.com>
2017-07-06 22:43:37 +0800

    整合ydui

--- MarineFuellingStation/ClientApp/boot.ts
+++ MarineFuellingStation/ClientApp/boot.ts
@@ -1,12 +1,16 @@
 import 'bootstrap';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import YDUI from 'vue-ydui';
+
+Vue.use(YDUI);
 Vue.use(VueRouter);
 
 const routes = [
     { path: '/', component: require('./components/home/home.vue.html') },
     { path: '/counter', component: require('./components/counter/counter.vue.html') },
-    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
+    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') },
+    { path: '/ydui', component: require('./components/ydui/ydui.vue.html') }
 ];
 
 new Vue({
---
+++ MarineFuellingStation/ClientApp/components/ydui/ydui.ts
@@ -0,0 +1,11 @@
+﻿import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import 'vue-ydui/dist/ydui.px.css';
+
+export default {
+    methods: {
+        handleClick() {
+            this.$dialog.alert({ mes: 'Hello World!' });
+        }
+    }
+}

9be223118bce3a91aeb629ff35e53a2e5d3e4b6d
一索 <yisuo_hjy@163.com>
2017-07-07 20:21:20 +0800

    油仓ui

--- MarineFuellingStation/ClientApp/boot.ts
+++ MarineFuellingStation/ClientApp/boot.ts
@@ -7,6 +7,7 @@ Vue.use(YDUI);
 Vue.use(VueRouter);
 
 const routes = [
+    { path: '/report/oilstore', component: require('./components/report/oilstore/oilstore.vue.html') },
     { path: '/', component: require('./components/home/home.vue.html') },
     { path: '/counter', component: require('./components/counter/counter.vue.html') },
     { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') },
---
+++ MarineFuellingStation/ClientApp/components/report/oilstore/oilstore.ts
@@ -0,0 +1,11 @@
+﻿import Vue from 'vue';
+import { ProgressBar } from 'vue-ydui/dist/lib.rem/progressbar';
+
+export default {
+    data() {
+        return {
+            progress1: 0.1,
+            progress2: 0.4
+        }
+    }
+}

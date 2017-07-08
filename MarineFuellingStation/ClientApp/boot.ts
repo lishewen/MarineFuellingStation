784d1b2e9c4661e7583498e9f738a86265f9e6d0
一索 <yisuo_hjy@163.com>
2017-07-08 16:53:30 +0800

    计划开单ui

--- MarineFuellingStation/ClientApp/boot.ts
+++ MarineFuellingStation/ClientApp/boot.ts
@@ -8,6 +8,7 @@ Vue.use(VueRouter);
 
 const routes = [
     { path: '/report/oilstore', component: require('./components/report/oilstore/oilstore.vue.html') },
+    { path: '/order/plan', component: require('./components/order/plan.vue.html') },
     { path: '/', component: require('./components/home/home.vue.html') },
     { path: '/counter', component: require('./components/counter/counter.vue.html') },
     { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') },
---
+++ MarineFuellingStation/ClientApp/components/order/plan.ts
@@ -0,0 +1,16 @@
+﻿import Vue from 'vue';
+import { Input } from 'vue-ydui/dist/lib.rem/input';
+import { CellGroup, CellItem } from 'vue-ydui/dist/lib.rem/cell';
+import { Radio, RadioGroup } from 'vue-ydui/dist/lib.rem/radio';
+import { CheckBox, CheckBoxGroup } from 'vue-ydui/dist/lib.rem/checkbox';
+import { Switch } from 'vue-ydui/dist/lib.rem/switch';
+import { Button, ButtonGroup } from 'vue-ydui/dist/lib.rem/button';
+import { Tab, TabPanel } from 'vue-ydui/dist/lib.rem/tab';
+
+export default {
+    data() {
+        return {
+            radio2: '1'
+        }
+    }
+}

ff37ff72065edb3c380c30c741bfb05aa9fc1f67
黎摄文 <lishewen@gmail.com>
2017-07-09 00:53:37 +0800

    css收纳为文件

--- MarineFuellingStation/ClientApp/components/app/app.ts
+++ MarineFuellingStation/ClientApp/components/app/app.ts
@@ -1,5 +1,11 @@
-import Vue from 'vue';
+﻿import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
+@Component
 export default class AppComponent extends Vue {
+    title: string = 'XXX油站管理系统';
+
+    setTitle(title: string): void {
+        this.title = title;
+    }
 }
--- MarineFuellingStation/ClientApp/components/order/plan.ts
+++ MarineFuellingStation/ClientApp/components/order/plan.ts
@@ -1,16 +1,7 @@
 ﻿import Vue from 'vue';
-import { Input } from 'vue-ydui/dist/lib.rem/input';
-import { CellGroup, CellItem } from 'vue-ydui/dist/lib.rem/cell';
-import { Radio, RadioGroup } from 'vue-ydui/dist/lib.rem/radio';
-import { CheckBox, CheckBoxGroup } from 'vue-ydui/dist/lib.rem/checkbox';
-import { Switch } from 'vue-ydui/dist/lib.rem/switch';
-import { Button, ButtonGroup } from 'vue-ydui/dist/lib.rem/button';
-import { Tab, TabPanel } from 'vue-ydui/dist/lib.rem/tab';
+import { Component } from 'vue-property-decorator';
 
-export default {
-    data() {
-        return {
-            radio2: '1'
-        }
-    }
+@Component
+export default class PlanComponent extends Vue {
+    radio2: string = '1';
 }

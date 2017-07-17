951dcfca6b1fd79801d3fd3305ccca210b21a611
黎摄文 <lishewen@gmail.com>
2017-07-18 04:46:02 +0800

    配置数据库连接

--- MarineFuellingStation/Startup.cs
+++ MarineFuellingStation/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using MFS.Models;
 
 namespace MFS
@@ -29,6 +30,10 @@ namespace MFS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<EFContext>(option =>
+            {
+                option.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]);
+            });
             // Add framework services.
             services.AddMvc();
 

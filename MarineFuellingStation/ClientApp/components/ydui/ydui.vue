3a02af74b4bd33ddc9a84703d4c9fa7e5d24db90
算神 <lishewen@msn.com>
2017-07-24 15:29:00 +0800

    引入百度echarts组件，演示在ydui.ts & ydui.vue

--- MarineFuellingStation/ClientApp/components/ydui/ydui.vue
+++ MarineFuellingStation/ClientApp/components/ydui/ydui.vue
@@ -2,6 +2,7 @@
     <div id="root">
         <weui-search v-model="sv" />
         <yd-button type="primary" @click.native="handleClick">Click Me!</yd-button>
+        <chart :options="polar"></chart>
     </div>
 </template>
 

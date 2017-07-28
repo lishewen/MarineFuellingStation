7ad202abc6ce7b08afaee8e99b3e7afe6c5fff82
算神 <lishewen@msn.com>
2017-07-28 09:34:35 +0800

    获取员工列表

--- MarineFuellingStation/ClientApp/components/user/user.ts
+++ MarineFuellingStation/ClientApp/components/user/user.ts
@@ -1,27 +1,35 @@
 ﻿import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import axios from "axios";
 @Component({
     components: {
         WeuiSearch: require('../weui-search/search.vue')
     }
 })
-export default class MyOrderComponent extends Vue {
+export default class UserComponent extends Vue {
     radio1: string = "1";
     radio2: string = "1";
     show1: boolean = false;
     carNo: string = "";
     sv: string = "";
+    users: work.userlist[];
     myItems1: object = [
         {
             label: '致电13907741118？',
             method: () => {
                 window.location.href = 'tel: 13907741118'
             }
-        },{
+        }, {
             label: '详细资料'
         }
     ];
-   
+
+    constructor() {
+        super();
+
+        this.users = new Array();
+    }
+
     mounted() {
         this.$emit('setTitle', this.$store.state.username + ' 员工资料');
     };
@@ -29,5 +37,15 @@ export default class MyOrderComponent extends Vue {
     change(label: string, tabkey: string) {
         console.log(label);
         this.$emit('setTitle', this.$store.state.username + ' ' + label);
+        if (label == '列表')
+            this.getUsers();
+    }
+
+    getUsers() {
+        axios.get('/api/User').then((res) => {
+            let jobj = res.data as work.departmentMemberInfoResult;
+            if (jobj.errcode == 0)
+                this.users = jobj.userlist;
+        });
     }
 }
--- MarineFuellingStation/ClientApp/components/user/user.vue
+++ MarineFuellingStation/ClientApp/components/user/user.vue
@@ -107,28 +107,17 @@
                 </div>
             </yd-tab-panel>
             <yd-tab-panel label="列表">
-
                 <yd-cell-group>
                     <weui-search v-model="sv" />
-                    <yd-cell-item arrow @click.native="show1 = true">
+                    <yd-cell-item arrow @click.native="show1 = true" v-for="user in users">
                         <div slot="left">
-                            <p>张三</p>
-                            <p style="color:lightgray;font-size:12px">普通职员</p>
+                            <p>{{user.name}}</p>
+                            <p style="color:lightgray;font-size:12px" v-show="user.isleader==0">普通职员</p>
+                            <p style="color:lightgray;font-size:12px" v-show="user.isleader==1">上级</p>
                         </div>
                         <div slot="right" style="text-align: right;margin-right: 5px">
                             <p>
-                                <span style="color:gray; font-size: 18px">销售部</span>
-                            </p>
-                        </div>
-                    </yd-cell-item>
-                    <yd-cell-item arrow @click.native="show1 = true">
-                        <div slot="left">
-                            <p>李四</p>
-                            <p style="color:lightgray;font-size:12px">上级</p>
-                        </div>
-                        <div slot="right" style="text-align: right;margin-right: 5px">
-                            <p>
-                                <span style="color:gray; font-size: 18px">生产部</span>
+                                <span style="color:gray; font-size: 18px">{{user.department[0]}}</span>
                             </p>
                         </div>
                     </yd-cell-item>

42dde5c9d9fa9591355d6af87ecdbdaa15a908cd
算神 <lishewen@msn.com>
2017-07-28 15:15:25 +0800

    客户 公司 初步建模

---
+++ MarineFuellingStation/Models/Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MFS.Models
+{
+    public class Client : EntityBase
+    {
+        public PlaceType PlaceType { get; set; }
+        public ClientType ClientType { get; set; }
+        public int? CompanyId { get; set; }
+        /// <summary>
+        /// 跟进销售
+        /// </summary>
+        public string FollowSalesman { get; set; }
+        public int DefaultProductId { get; set; }
+        /// <summary>
+        /// 联系人
+        /// </summary>
+        public string Contact { get; set; }
+        public string Mobile { get; set; }
+        public string IdCard { get; set; }
+        public string Address { get; set; }
+        /// <summary>
+        /// 固定电话
+        /// </summary>
+        public string Phone { get; set; }
+        /// <summary>
+        /// 最高挂账金额
+        /// </summary>
+        public decimal MaxOnAccount { get; set; }
+        /// <summary>
+        /// 总消费金额
+        /// </summary>
+        public decimal TotalAmount { get; set; }
+
+    }
+    public enum PlaceType
+    {
+        陆上,
+        水上
+    }
+    public enum ClientType
+    {
+        个人,
+        公司
+    }
+}
---
+++ MarineFuellingStation/Models/Company.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MFS.Models
+{
+    public class Company : EntityBase
+    {
+        public TicketType TicketType { get; set; }
+        /// <summary>
+        /// 发票抬头
+        /// </summary>
+        public string InvoiceTitle { get; set; }
+        /// <summary>
+        /// 税号
+        /// </summary>
+        public string TaxFileNumber { get; set; }
+        /// <summary>
+        /// 对公账户
+        /// </summary>
+        public string BusinessAccount { get; set; }
+        /// <summary>
+        /// 开户银行
+        /// </summary>
+        public string Bank { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        /// <summary>
+        /// 账户余额
+        /// </summary>
+        public decimal Balances { get; set; }
+        /// <summary>
+        /// 总消费金额
+        /// </summary>
+        public decimal TotalAmount { get; set; }
+    }
+    public enum TicketType
+    {
+        普通票,
+        专用票
+    }
+}

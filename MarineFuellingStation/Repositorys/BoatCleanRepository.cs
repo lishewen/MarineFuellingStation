2ebe2dad14ebefc5e379277a7a460e143a75a6b3
算神 <lishewen@msn.com>
2017-09-06 16:12:22 +0800

    带有标头的单号生成实现

--- MarineFuellingStation/Repositorys/BoatCleanRepository.cs
+++ MarineFuellingStation/Repositorys/BoatCleanRepository.cs
@@ -8,6 +8,36 @@ namespace MFS.Repositorys
 {
     public class BoatCleanRepository : RepositoryBase<BoatClean>
     {
+        const string tag = "QW";
         public BoatCleanRepository(EFContext dbContext) : base(dbContext) { }
+        public string GetLastBoatCleanNo()
+        {
+            try
+            {
+                return GetAllList().OrderByDescending(o => o.Id).First().Name;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+        public string GetSerialNumber(string serialNumber = "0")
+        {
+            if (!string.IsNullOrWhiteSpace(serialNumber) && serialNumber != "0")
+            {
+                if (serialNumber.Length == 10)
+                {
+                    string headDate = serialNumber.Substring(2, 4);
+                    int lastNumber = int.Parse(serialNumber.Substring(6));
+                    //如果数据库最大值流水号中日期和生成日期在同一天，则顺序号加1
+                    if (headDate == DateTime.Now.ToString("yyMM"))
+                    {
+                        lastNumber++;
+                        return tag + headDate + lastNumber.ToString("0000");
+                    }
+                }
+            }
+            return tag + DateTime.Now.ToString("yyMM") + "0001";
+        }
     }
 }

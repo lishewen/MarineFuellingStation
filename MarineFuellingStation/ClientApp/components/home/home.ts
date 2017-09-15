bc1da4dc62a0d6c210e0e4ac61ddddec2652b89c
算神 <lishewen@msn.com>
2017-09-15 14:39:05 +0800

    人为补充 ydui $dialog 的 ts 定义

--- MarineFuellingStation/ClientApp/components/home/home.ts
+++ MarineFuellingStation/ClientApp/components/home/home.ts
@@ -4,11 +4,10 @@ import { Component } from 'vue-property-decorator';
 @Component
 export default class HomeComponent extends Vue {
     mounted() {
-        let that = <any>this;
-        that.$dialog.loading.open('很快加载好了');
+        this.$dialog.loading.open('很快加载好了');
 
         setTimeout(() => {
-            that.$dialog.loading.close();
+            this.$dialog.loading.close();
         }, 2000);
     }
 }

ccc21c19d82c4ca156f64e33c133b05a433ab14b
黎摄文 <lishewen@gmail.com>
2017-09-23 06:36:51 +0800

    清理TS错误

--- MarineFuellingStation/Models/InAndOutLog.cs.d.ts
+++ MarineFuellingStation/Models/InAndOutLog.cs.d.ts
@@ -1,22 +1,9 @@
 declare module server {
 	/** 出入仓记录 */
-	interface inAndOutLog extends EntityBase {
-		type: any;
-		storeId: number;
-		store: {
-			storeClass: any;
-			volume: number;
-			avgPrice: number;
-			lastValue: number;
-			value: number;
-			lastSurveyAt: Date;
-			cost: number;
-			isUse: boolean;
-			storeTypeId: number;
-			storeType: {
-				stores: any[];
-			};
-		};
+    interface inAndOutLog extends entityBase {
+        type: logType;
+        storeId: number;
+        store: store;
 		/** 操作员 | 分隔 */
 		operators: string;
 		/** 单位 */

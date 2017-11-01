6be35c602ada3db898f5a5a5905ee098e1770ee6
算神 <lishewen@msn.com>
2017-11-02 07:47:30 +0800

    化验仓名显示问题

--- MarineFuellingStation/ClientApp/components/produce/assay.vue
+++ MarineFuellingStation/ClientApp/components/produce/assay.vue
@@ -122,7 +122,7 @@
                             <p style="color:lightgray">{{s.assayer}}</p>
                         </div>
                         <div slot="right" style="text-align: left;margin-right: 5px">
-                            <p v-show="s.assayType == 0" style="color:darkgreen">{{s.store == null?"":s.store}}仓</p>
+                            <p v-show="s.assayType == 0" style="color:darkgreen">{{s.store == null?"":s.store.name}}仓</p>
                             <p v-show="s.assayType == 1" style="color:lightcoral">{{s.purchase == null?"":s.purchase.carNo}} / {{s.purchase == null?"":s.purchase.count}}吨</p>
                         </div>
                     </yd-cell-item>

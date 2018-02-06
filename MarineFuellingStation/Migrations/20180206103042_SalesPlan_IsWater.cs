523a2cb058ed85dddf15be6a52a825edc895aa2e
继业 黄 <yisuo_hjy@163.com>
2018-02-06 18:31:30 +0800

    水上陆上标识

---
+++ MarineFuellingStation/Migrations/20180206103042_SalesPlan_IsWater.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MFS.Migrations
+{
+    public partial class SalesPlan_IsWater : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWater",
+                table: "SalesPlans",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsWater",
+                table: "SalesPlans");
+        }
+    }
+}

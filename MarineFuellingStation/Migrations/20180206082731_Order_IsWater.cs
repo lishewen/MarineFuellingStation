7f1a0c64f2df4f9a283a0d1624ca2f10b26dfc7f
继业 黄 <yisuo_hjy@163.com>
2018-02-06 16:28:35 +0800

    IsWater标识

---
+++ MarineFuellingStation/Migrations/20180206082731_Order_IsWater.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MFS.Migrations
+{
+    public partial class Order_IsWater : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsWater",
+                table: "Orders",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsWater",
+                table: "Orders");
+        }
+    }
+}
